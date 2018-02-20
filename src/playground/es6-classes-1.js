f6766ec90422e726982ab8538c3ac52d5abc8137
Vladimir Gasic <vgashic@gmail.com>
2018-02-20 07:00:33 +0100

    after lesson #25 challenge

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -15,6 +15,56 @@ class Person {
 
 }
 
-const me = new Person('Gasha', 38);
 
-console.log(me.getGreeting(), me.getDescription());
+class Student extends Person {
+
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+
+	hasMajor() {
+		return !!this.major;
+	}
+
+	getDescription() {
+		let description = super.getDescription();
+
+		if (this.hasMajor()) {
+			description += ` Their major is ${this.major}.`
+		}
+
+		return description;
+	}
+}
+
+
+class Traveler extends Person {
+
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+
+	getDescription() {
+
+		let description = `Hi I am ${this.name}.`;
+
+		if (!!this.homeLocation) {
+			description += ` I'm visiting from ${this.homeLocation}. `
+		}
+
+		return description;
+	}
+
+}
+
+
+
+const me = new Traveler('Gasha', 38, 'Belgrade');
+console.log(me.getDescription());
+
+const other = new Traveler()
+console.log(other.getDescription());
+
+
